d50935206ee775faedeeb337693a9a7396ebeab5
sadeqsalehi <sadeqsalehi@gmail.com>
2019-03-30 17:21:01 +0430

    Optimized the list and search City within Wikipedia.

--- app/src/main/java/ir/sadeqsalehi/customlistview/CityAdapter.java
+++ app/src/main/java/ir/sadeqsalehi/customlistview/CityAdapter.java
@@ -1,6 +1,8 @@
 package ir.sadeqsalehi.customlistview;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -10,8 +12,10 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
+import java.util.concurrent.TimeoutException;
 
 public class CityAdapter extends ArrayAdapter {
 
@@ -26,16 +30,46 @@ public class CityAdapter extends ArrayAdapter {
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         City city = cities.get(position);
-        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view = inflater.inflate(R.layout.city_list_item, parent, false);
-        ImageView cityImage = view.findViewById(R.id.imgCityImage);
-        TextView cityName = view.findViewById(R.id.tvCityName);
-        TextView cityPopulation = view.findViewById(R.id.tvCityPopulation);
-        if (city.getId() != 0) {
-            cityImage.setImageResource(city.getId());
+        ViewHolder holder;
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.city_list_item, parent, false);
+            holder = new ViewHolder();
+            holder.cityImage = convertView.findViewById(R.id.imgCityImage);
+            holder.cityName = convertView.findViewById(R.id.tvCityName);
+            holder.cityPopulation = convertView.findViewById(R.id.tvCityPopulation);
+            holder.imgSearch = convertView.findViewById(R.id.imgSearch);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+
         }
-        cityName.setText(city.getName());
-        cityPopulation.setText(String.valueOf(city.getPopulation()));
-        return view;
+        holder.fill(city);
+        return convertView;
+    }
+
+    private class ViewHolder implements View.OnClickListener {
+        ImageView cityImage;
+        TextView cityName;
+        TextView cityPopulation;
+        ImageView imgSearch;
+
+        public void fill(City city) {
+            if (city.getId() != 0) {
+                cityImage.setImageResource(city.getId());
+            }
+            cityName.setText(city.getName());
+            cityPopulation.setText(String.valueOf(city.getPopulation()));
+            imgSearch.setTag(city.getName());
+            imgSearch.setOnClickListener(this);
+        }
+        @Override
+        public void onClick(View v) {
+            String uri= "https://ar.wikipedia.org/wiki/"+v.getTag().toString();
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(uri));
+            getContext().startActivity(intent);
+           // Toast.makeText(getContext(), "Search for "+v.getTag().toString(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/ir/sadeqsalehi/customlistview/CustomListViewActivity.java
+++ app/src/main/java/ir/sadeqsalehi/customlistview/CustomListViewActivity.java
@@ -13,36 +13,47 @@ public class CustomListViewActivity extends AppCompatActivity {
     List<City> cities;
     ListView citiesListView;
     CityAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        citiesListView =findViewById(R.id.lstCities);
-        cities= new ArrayList<>();
+        citiesListView = findViewById(R.id.lstCities);
+        cities = new ArrayList<>();
         populateCitiesList();
         refreshDisplay();
 
     }
 
     private void refreshDisplay() {
-           adapter= new CityAdapter(this,cities);
-            citiesListView.setAdapter(adapter);
-   }
-    private void populateCitiesList() {
-                cities.add(new City("Amsterdam",12000000,R.drawable.amsterdam));
-                cities.add(new City("Austin",800000,R.drawable.austin));
-                cities.add(new City("Barcelona",2000000,R.drawable.barcelona));
-                cities.add(new City("Berlin",3500000,R.drawable.berlin));
-                cities.add(new City("Cape Town",6700000,R.drawable.cape_town));
-                cities.add(new City("Dublin",3400000,R.drawable.dublin));
-                cities.add(new City("London",1000000,R.drawable.london));
-                cities.add(new City("New York",14000000,R.drawable.new_york));
-                cities.add(new City("Paris",5800000,R.drawable.paris));
-                cities.add(new City("San Francisco",9500000,R.drawable.san_francisco));
-                cities.add(new City("Stockholm",2300000,R.drawable.stockholm));
-                cities.add(new City("Sydney",4100000,R.drawable.sydney));
-                cities.add(new City("Tokyo",7800000,R.drawable.tokyo));
-                cities.add(new City("Wellington",300000,R.drawable.wellington));
+        adapter = new CityAdapter(this, cities);
+        citiesListView.setAdapter(adapter);
+    }
 
+    private void populateCitiesList() {
+        //addFakeCities(8);
+        cities.add(new City("Amsterdam", 12000000, R.drawable.amsterdam));
+        cities.add(new City("Austin", 800000, R.drawable.austin));
+        cities.add(new City("Barcelona", 2000000, R.drawable.barcelona));
+       // addFakeCities(8);
+        cities.add(new City("Berlin", 3500000, R.drawable.berlin));
+        cities.add(new City("Cape Town", 6700000, R.drawable.cape_town));
+        cities.add(new City("Dublin", 3400000, R.drawable.dublin));
+        cities.add(new City("London", 1000000, R.drawable.london));
+        //addFakeCities(8);
+        cities.add(new City("New York", 14000000, R.drawable.new_york));
+        cities.add(new City("Paris", 5800000, R.drawable.paris));
+        cities.add(new City("San Francisco", 9500000, R.drawable.san_francisco));
+        cities.add(new City("Stockholm", 2300000, R.drawable.stockholm));
+        //addFakeCities(4);
+        cities.add(new City("Sydney", 4100000, R.drawable.sydney));
+        cities.add(new City("Tokyo", 7800000, R.drawable.tokyo));
+        cities.add(new City("Wellington", 300000, R.drawable.wellington));
+
+    }
+
+    private void addFakeCities(int n) {
+        for (int i = 0; i < n; i++)
+            cities.add(new City("Amsterdam", 12000000, R.drawable.amsterdam));
     }
 }
